ec777657b68347b952433e638d9aa5e83936f87a
Yi Sun <sun@kic.ac.jp>
2024-11-29 12:02:29 +0200

    first init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/greet', (req, res) => {
+    res.send('Greet!');
+});
+
+app.get('/hello', (req, res) => {
+    res.send('Konnichiha');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
+});
